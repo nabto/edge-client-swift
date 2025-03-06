8ad46e3f3ffadee786873f544ec5dac62574cb4d
Ulrik Gammelby <ug@nabto.com>
2025-03-06 12:54:10 +0100

    test with 5.12.2

--- Package.swift
+++ Package.swift
@@ -20,8 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "NabtoEdgeClientApi",
-            url: "https://downloads.nabto.com/assets/edge/ios/api/0.0.5-nsurldebug/NabtoEdgeClientApiFW.xcframework.zip",
-            checksum: "9cdf35eb9438d0b010a074a70e5aeffe3e43fc75fcb9262f015b3dbb60aa8f2e"),
+//            url: "https://downloads.nabto.com/assets/edge/ios/api/0.0.5-nsurldebug/NabtoEdgeClientApiFW.xcframework.zip",
+            url: "https://downloads.nabto.com/assets/edge/ios/api/5.12.2-rc1/NabtoEdgeClientApiFW.xcframework.zip",
+//            checksum: "9cdf35eb9438d0b010a074a70e5aeffe3e43fc75fcb9262f015b3dbb60aa8f2e"),
+            checksum: "5ea569f5f51cbcc80719d645ea10a81cfe4222efe03f7c8a3ff6f05b353f04bf"),
+
         .target(
             name: "NabtoEdgeClient",
             dependencies: ["NabtoEdgeClientApi", "CBORCoding"],
