5f1b8dc043164f0f340879ee1523c9b4525f3ed2
Ulrik Gammelby <ug@nabto.com>
2025-03-06 14:20:31 +0100

    test with nsurl-debug nabto-client-sdk

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "NabtoEdgeClientApi",
-            url: "https://downloads.nabto.com/assets/edge/ios/api/0.0.7-nsurldebug/NabtoEdgeClientApiFW.xcframework.zip",
-            checksum: "78990743fcc058a170e1610afeff4a50f715b2ef952f5c0c9b45f8b9e1658b86"),
+            url: "https://downloads.nabto.com/assets/edge/ios/api/0.0.8-nsurldebug/NabtoEdgeClientApiFW.xcframework.zip",
+            checksum: "0f5dc27a9e8ce11fb5deb9caf3f04ac85eeffbcba5154c183c6d82471db8266c"),
 //            url: "https://downloads.nabto.com/assets/edge/ios/api/0.0.5-nsurldebug/NabtoEdgeClientApiFW.xcframework.zip",
 //            checksum: "9cdf35eb9438d0b010a074a70e5aeffe3e43fc75fcb9262f015b3dbb60aa8f2e"),
         .target(
